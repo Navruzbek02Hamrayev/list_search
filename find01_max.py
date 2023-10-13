9cef9b3308be9475d119e3962a50fcb045e21cae
Navruzbek02Hamrayev <hamrayevnavruz12@gmail.com>
2023-10-13 07:16:34 -0400

    Return the max

--- find01_max.py
+++ find01_max.py
@@ -5,7 +5,13 @@ def find_max(data):
         data: list of numbers
     returns: maximum number in the list
     """
-    return max(data)
+    i=0
+    max=data[0]
+    while i<len(data):
+        if max<data[i]:
+            max=data[i]
+        i+=1
+    return max
 print(find_max([1, 2, 3, 4, 5]))
 print(find_max([12, 2, 5, 2, 7, 9, 1]))
     
