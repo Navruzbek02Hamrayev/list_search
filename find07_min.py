d63c3f8a4b1767a2b1ba534f0c94bcf3c3b0b692
Navruzbek02Hamrayev <hamrayevnavruz12@gmail.com>
2023-10-14 00:33:56 -0400

    Return the min

--- find07_min.py
+++ find07_min.py
@@ -5,5 +5,12 @@ def find_min(data):
         data: list of numbers
     returns: minimum number in the list
     """
-
-    return 0
+    i=0
+    min=data[0]
+    while i<len(data):
+        if min>data[i]:
+            min=data[i]
+        i+=1
+    return min
+print(find_min([1, 2, -3, 4, 5]))
+print(find_min([15, 23, 3, 9, 1, 4]))
